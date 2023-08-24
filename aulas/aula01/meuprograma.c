d1420557896d30bb49486659066b8d110181ed30
alexandregcastr <25529755-alexandregcastr@users.noreply.replit.com>
2023-08-24 00:46:11 +0000

    arquivos da aula 01

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,17 @@
+/*
+Autor: Alexandre Castro
+Data: 23/08/2023
+
+Programa que existe Ola Pessoal!
+  */
+#include <math.h>
+  
+#include <stdio.h>
+
+// funcao principal
+int main () {
+    printf( "ola pessoal!\n");
+ 
+  printf("A raiz quadrada de 2 é %f", sqrt(2));
+  return 0;
+}
